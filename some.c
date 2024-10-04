8dbe9e90fc054565c9b24778b9b4e9030941f778
Mitesh Rajdev <mrajdev346@rku.ac.in>
2024-10-04 13:18:18 +0530

    new commite

---
+++ some.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+
+int main()
+{
+ /*
+    int a;
+    int b;
+    
+    
+    printf("Enter number a");
+    scanf("%d", &a);
+     
+    printf("Enter Number b");
+    scanf("%d", &b);
+
+     int sum = a+b;
+     printf("%d",sum);
+    
+
+    char name ;
+    int age ;
+    char address;
+    int contactno; 
+
+     printf("enter Your Name:");
+     scanf("%c/n",&name);
+     printf("name is : %c",name);
+
+     printf("enter Your Age:");
+     scanf("%d/n",&age);
+     printf("%d",age);
+
+     printf("enter Your Address:");
+     scanf("%c/n",&address);
+     printf("%c",address);
+
+     printf("enter Your contactno:");
+     scanf("%d/n",&contactno);
+     printf("%d",contactno);
+
+
+    // using Oprator
+
+       int x;
+
+       printf("Enter Number :");
+       scanf("%d",x);
+       printf("%d",x/2 == 0);  
+   
+
+       int a =20;
+       int b =40;
+       int c =60;
+       int sum = (a+b+c)/3;
+
+        printf("%d",sum);
+  
+    
+
+        int x;
+
+        printf("enter Character :");
+        scanf("%d",x);
+        if (x >= '0' && x <= '9' )
+        {
+            printf("it is a digital no : %d",x);
+        } else  {
+            printf("it is Not digital No: %d", x);
+        }
+        
+             return 0;
+
+    
+
+    int marks;
+    printf("enter Your Marks");
+    scanf("%d",&marks);
+    if(marks>30)
+    {
+        printf("Student is Pass");
+    }
+
+    else
+    {
+        printf("student is fail");
+    }
+*/ 
+
+}
